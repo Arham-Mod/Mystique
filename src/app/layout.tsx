cfd71abda5de5ad06a762a66243355b3a710d3c4
Mohd Arham <mdarham2405@gmail.com>
2025-08-09 23:19:46 +0530

    gray box_1

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,12 @@
 import "./globals.css";
 import Navbar from "./components/Navbar";
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body>
-        <Navbar /> 
-        {children}
+        <Navbar />
+        <main>{children}</main>
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
     <div>
+      <div className="box w-240 h-200 bg-[#EAEFEE]">
+      </div>
+      <img src=".\public\Home_cloth.jpeg"  /> //to do
     </div>
   );
 }
