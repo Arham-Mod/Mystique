caf8f5691f2f6edd59956fae4f12da8a8c8ac282
Mohd Arham <mdarham2405@gmail.com>
2025-08-09 18:47:21 +0530

    nav

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,2 +1,14 @@
-export default function Navbar(){
-};
+export default function Navbar() {
+  return (
+    <nav className="relative px-4 py-4 border-b border-black-500">
+      <div className="flex items-center">
+        <div className="flex space-x-4">
+          <a href="#" className="text-black font-medium text-[22px] hover:text-gray-700">Male</a>
+          <a href="#" className="text-black font-medium text-[22px] hover:text-gray-700">Female</a>
+          <a href="#" className="text-black font-medium text-[22px] hover:text-gray-700">Collections</a>
+        </div>
+      </div>
+      <h1 className="absolute left-1/2 -translate-x-1/2 top-1/2 -translate-y-1/2 text-[60px] font-black tracking-[-0.4rem]">VISTRY</h1>
+    </nav>
+  );
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,8 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <body>
-        <Navbar /> //to do 
+        <Navbar /> 
+        {children}
       </body>
     </html>
   );
