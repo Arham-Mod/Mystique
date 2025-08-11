c63ad09b609b69509eac47125517a79cc4e023a8
Mohd Arham <mdarham2405@gmail.com>
2025-08-11 23:54:10 +0530

    successful (but only on my device

--- src/app/components/Cover_image.tsx
+++ src/app/components/Cover_image.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image';
 
 export default function CoverImage() {
   return (
-    <div className="absolute right-0 top-0 w-[200px] h-[200px]"> {/* Stick to right top; adjust size */}
+    <div className="absolute right-0 top-0 w-235 h-200"> {/* Stick to right top; adjust size */}
       <Image
         src="/Home_cloth.jpeg"
         alt="Cover image for VISTRY"
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import CoverImage from "./components/Cover_image";
 export default function Home() {
   return (
     <div className="flex h-screen"> {/* Full height flex */}
-      <div className="w-1/2 bg-[#EAEFEE]"></div> {/* Left gray */}
+      <div className="w-1/2 h-200 bg-[#EAEFEE]"></div> {/* Left gray */}
       <div className="w-1/2 bg-white relative"> {/* Right white with relative for absolute child */}
         <CoverImage /> {/* Image positions absolute right */}
       </div>
