2a7bd18a742d3d3a41be21b5557efcc3bc471b5d
Mohd Arham <mdarham2405@gmail.com>
2025-08-14 01:35:16 +0530

    Changed page to gridbox

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,16 @@ import CoverImage from "./components/Cover_image";
 
 export default function Home() {
   return (
-    <div className="flex h-screen">
-      <div className="w-1/2 h-240 bg-[#EAEFEE] flex items-center justify-center">
+    <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
+      <div className=" h-240 bg-[#EAEFEE] flex items-center justify-center">
         <h2 className="text-[174px] tracking-[-0.4rem] font-bold rotate-90 text-black flex flex-col items-center">
-          <span className="translate-y-[-270px] translate-x-[-50px]">BEYOND</span> {/* Example: move up */}
-          <span className="translate-y-[-370px] translate-x-[-140px]">STYLE</span> {/* Example: move down */}
+          <span className="translate-y-[-270px] translate-x-[-50px]">BEYOND
+            </span> {/* Example: move up */}
+          <span className="translate-y-[-370px] translate-x-[-140px]">STYLE
+            </span> {/* Example: move down */}
         </h2>
       </div>
-      <div className="w-1/3 h-full bg-white relative">
+      <div className=" right-0 h-full bg-white relative">
         <CoverImage /> {/* Image positions absolute right */}
       </div>
     </div>
