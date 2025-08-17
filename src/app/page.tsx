c9ce9af1882fe9673d5eb2dc51d2c0baffaaf0a2
Mohd Arham <mdarham2405@gmail.com>
2025-08-18 00:26:31 +0530

    VISTRY headline partially complete

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,9 +7,9 @@ export default function Home() {
       <div className=" h-240 bg-[#EAEFEE] flex items-center justify-center">
         <h2 className="text-[174px] tracking-[-0.4rem] font-bold rotate-90 text-black flex flex-col items-center">
           <span className="translate-y-[-270px] translate-x-[-50px]">BEYOND
-            </span> {/* Example: move up */}
+            </span> 
           <span className="translate-y-[-370px] translate-x-[-140px]">STYLE
-            </span> {/* Example: move down */}
+            </span> 
         </h2>
       </div>
         <div className=" w-full h-full relative right-0">
@@ -20,7 +20,7 @@ export default function Home() {
           </div>
       </div>
       <div>
-        <h1><span className="V-font">V</span><span className="ISTRY-font">ISTRY</span></h1>
+        <h1><span className="V-font">V</span><span className="ISTRY-font">ISTRY</span></h1>{/*Minor adjustment of text needed (have to increase the spacing between v and istry) */}
       </div>
     </div>
 
