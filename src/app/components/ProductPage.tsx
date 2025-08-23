1d8cd7238280b2e658e9ed3c71c5b2c17a6b8d52
Mohd Arham <mdarham2405@gmail.com>
2025-08-24 01:10:03 +0530

    new page added - PRODUCT

---
+++ src/app/components/ProductPage.tsx
@@ -0,0 +1,8 @@
+export default function Product() {
+    return(
+        <div>
+            <h1>hello</h1>
+        </div>
+    );
+};
+{/*figure out about the problem in app routing cause this page is not being displayed in the website */}
