87eebc32c55ab4f97247a5578494eca26149bb08
Mohd Arham <mdarham2405@gmail.com>
2025-08-25 16:35:00 +0530

    page directory change

---
+++ src/app/ProductPage.tsx
@@ -0,0 +1,12 @@
+export default function Product() {
+    return(
+        <div className="flex h-screen">
+            <div className="w-2/3 border-r border-gray-500 flex flex-col">
+                <div className="w-full h-1/3 bg-gray-200">Item 1</div>
+                <div className="w-full h-1/3 bg-gray-300">Item 2</div>
+                <div className="w-full h-1/3 bg-gray-400">Item 3</div>
+            </div>
+        </div>
+    );
+};
+{/*figure out about the problem in app routing cause this page is not being displayed in the website */}
--- src/app/components/ProductPage.tsx
+++
@@ -1,9 +0,0 @@
-export default function Product() {
-    return(
-        <div>
-            <h1>hello</h1>
-            <h2>Product page</h2>
-        </div>
-    );
-};
-{/*figure out about the problem in app routing cause this page is not being displayed in the website */}
