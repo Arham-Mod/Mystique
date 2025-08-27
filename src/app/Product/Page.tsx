8177215b6f7d20d0355eb9d0afbf038b20549733
Mohd Arham <mdarham2405@gmail.com>
2025-08-27 23:50:50 +0530

    formatting of code

---
+++ src/app/Product/Page.tsx
@@ -0,0 +1,13 @@
+export default function Product() {
+    return(
+        <div className="flex h-screen">
+            <div className="w-2/3  flex flex-row ">
+                <div className="w-full h-2/4 bg-gray-200">Item 1</div>
+                <div className="w-full h-2/4 bg-gray-300">Item 2</div>
+                <div className="w-full h-4/5 bg-gray-400">Item 3</div>
+                
+            </div>
+        </div>
+    );
+};
+{/*figure out about the problem in app routing cause this page is not being displayed in the website */}
