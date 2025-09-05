1a7c9d84f1dd3738b62c0b88fa08e8b1d24fb8e5
Mohd Arham <mdarham2405@gmail.com>
2025-09-06 03:24:06 +0530

    product page navigation correction

--- src/app/Product/[slug]/Page.tsx
+++ src/app/Product/[slug]/Page.tsx
@@ -1,16 +1,30 @@
 import Card from "@/app/components/Card";
 
-export default function Product() {
-    return(
-        <div className="flex h-screen">
-            <div className="w-2/3  flex flex-row">
-                <div className="w-full h-2/4 m-5 ">
-                    <Card slug="Cloth" imgSrc="/Card_clothes/sticker.webp" alt="Cloth " className={"Sticker"} />
-                </div>
-                <div className="w-full h-2/4 bg-gray-300 m-5 ">Item 2</div>
-                <div className="w-full h-4/5 bg-gray-400 m-5">Item 3</div>
-            </div>
+interface ProductPageProps {
+  params: {
+    slug: string;
+  };
+}
+
+export default function Product({ params }: ProductPageProps) {
+  const { slug } = params;
+  
+  return(
+    <div className="flex h-screen">
+      <div className="w-2/3 flex flex-row">
+        <div className="w-full h-2/5 m-5">
+          <Card 
+            slug={slug} 
+            imgSrc={`/Card_clothes/sticker${slug === 'Cloth1' ? '' : slug.replace('Cloth', '')}.webp`}
+            alt={`Product ${slug}`} 
+            className="Sticker" 
+          />
         </div>
-    );
-};
-{/*figure out about the problem in app routing cause this page is not being displayed in the website */}
+        <div className="w-full h-2/5 bg-gray-300 m-5">
+        </div>
+        <div className="w-full h-4/5 bg-gray-400 m-5">
+        </div>
+      </div>
+    </div>
+  );
+}
