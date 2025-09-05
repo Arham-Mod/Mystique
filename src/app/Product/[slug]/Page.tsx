954cc271c8cc09d1cc4bc8b2b955a6a65fdecd5c
Mohd Arham <mdarham2405@gmail.com>
2025-09-05 18:05:11 +0530

    added card into product page

--- src/app/Product/[slug]/Page.tsx
+++ src/app/Product/[slug]/Page.tsx
@@ -1,10 +1,14 @@
+import Card from "@/app/components/Card";
+
 export default function Product() {
     return(
         <div className="flex h-screen">
             <div className="w-2/3  flex flex-row">
-                <div className="w-full h-2/4 bg-gray-200 m-4 ">Item 1</div>
-                <div className="w-full h-2/4 bg-gray-300 ">Item 2</div>
-                <div className="w-full h-4/5 bg-gray-400">Item 3</div>
+                <div className="w-full h-2/4 m-5 ">
+                    <Card slug="Cloth" imgSrc="/Card_clothes/sticker.webp" alt="Cloth " className={"Sticker"} />
+                </div>
+                <div className="w-full h-2/4 bg-gray-300 m-5 ">Item 2</div>
+                <div className="w-full h-4/5 bg-gray-400 m-5">Item 3</div>
             </div>
         </div>
     );
